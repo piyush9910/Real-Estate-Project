5abf61b9a7f1836fe13c54771d7bfa92e670a5f8
piyush9910 <suripiyush4@gmail.com>
2023-05-28 15:38:58 +0530

    Registration Form Added and Data Storage in Local Storage

--- Frontend/Real-Estate-Project/src/app/property/add-property/add-property.component.ts
+++ Frontend/Real-Estate-Project/src/app/property/add-property/add-property.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-property',
@@ -7,13 +8,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-property.component.css']
 })
 export class AddPropertyComponent implements OnInit {
-
+  @ViewChild('Form') addPropertyForm: NgForm;
+  SellRent = '1';
   constructor(private router: Router) { }
 
   ngOnInit() {
+    // this.addPropertyForm.controls['Name'].setValue('Default Value');
+
+    setTimeout(() => {
+      this.addPropertyForm.controls['Name'].setValue('Default Value');
+    });
+
   }
-  onBack(){
-    this.router.navigate(['/'])
+
+  onBack() {
+    this.router.navigate(['/']);
+  }
+
+  onSubmit() {
+    console.log('Congrats, form Submitted');
+    console.log(this.addPropertyForm);
   }
 
 }
